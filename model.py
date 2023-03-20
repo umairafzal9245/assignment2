b71693a74fd1ccf8d7f6a29ff97acf68675a2a4b
umair afzal <i190517@nu.edu.pk>
2023-03-20 19:49:47 +0500

    first commit

---
+++ model.py
@@ -0,0 +1,26 @@
+from psx import stocks
+import datetime
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+
+data = stocks("SILK", start=datetime.date(2020, 1, 1), end=datetime.date.today())
+
+features = ["Open", "High", "Low"]
+target = "Close"
+
+X = data[features]
+y = data[target]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+print('LR Coefficients: \n', model.coef_)
+print('LR Intercept: \n', model.intercept_)
+
+y_test_pred = model.predict(X_test)
+
+print('Mean squared error: ',mean_squared_error(y_test, y_test_pred))
+print('Coefficient of determination: ',r2_score(y_test, y_test_pred))
